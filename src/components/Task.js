8d79d13c748486ea4c89b1eab86a82800d9bf534
serhatprogramming <serhatprogramming@gmail.com>
2023-08-27 22:29:21 -0400

    update mutation added.

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,18 @@
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { updateTask } from "../services/api";
+
 const Task = ({ task }) => {
+  const queryClient = useQueryClient();
+
+  const updateTaskMutation = useMutation(
+    (updatedTask) => updateTask(task.id, updatedTask),
+    {
+      onSuccess: () => queryClient.invalidateQueries("tasks"),
+    }
+  );
+
   const handleToggleUrgent = () => {
-    console.log("ToggleUrgent");
+    updateTaskMutation.mutate({ ...task, urgent: !task.urgent });
   };
 
   return (
