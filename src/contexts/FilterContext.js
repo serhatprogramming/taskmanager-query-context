a670d0b7a31549d110d906df5cb625a045da7173
serhatprogramming <serhatprogramming@gmail.com>
2023-08-27 23:04:46 -0400

    filter context added.

---
+++ src/contexts/FilterContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useReducer } from "react";
+
+const FilterContext = createContext();
+
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_FILTER":
+      return action.filter;
+    default:
+      return state;
+  }
+};
+
+export const FilterProvider = ({ children }) => {
+  const [filter, dispatch] = useReducer(filterReducer, "all");
+
+  return (
+    <FilterContext.Provider value={{ filter, dispatch }}>
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+export default FilterContext;
